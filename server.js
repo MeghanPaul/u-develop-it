5961f90f555c357c74d1a0913418bb0d7aef1265
Meghan <mlpaul4901@gmail.com>
2022-03-15 18:16:27 -0500

    testing querying from a javascript file

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//Express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user:'root',
+        password: 'frankieolive',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+})
+
+//catch all
 app.use((req,res)=> {
     res.status(404).end();
 });
